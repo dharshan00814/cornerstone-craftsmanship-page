6942c0933eeb1faf65d69d6fe39d7989cec82dd5
Dharshan <jmdharshan2@gmail.com>
2026-08-01 09:28:34 +0530

    commit

--- hack.js
+++ hack.js
@@ -115,24 +115,6 @@ observer.observe(el);
 });
 
 // ==========================
-// Mouse Glow
-// ==========================
-
-const glow = document.createElement("div");
-
-glow.className = "cursorGlow";
-
-document.body.appendChild(glow);
-
-document.addEventListener("mousemove",(e)=>{
-
-glow.style.left=e.clientX+"px";
-
-glow.style.top=e.clientY+"px";
-
-});
-
-// ==========================
 // Navbar Blur
 // ==========================
 
